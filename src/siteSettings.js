aa00a7c60795820cd37b4802900a8de9a5c2e71e
Vartika Jain <jainvartika2707@gmail.com>
2024-04-22 17:27:25 +0530

    change bot alias id for naigara falls

--- src/siteSettings.js
+++ src/siteSettings.js
@@ -36,7 +36,7 @@ module.exports = {
         "headerIcon": "https://prodv3-hornblower-assets.s3.us-west-2.amazonaws.com/data/chatbot/niagarafallsLogo.png",
         "chatBotIcon": "https://prodv3-hornblower-assets.s3.us-west-2.amazonaws.com/data/chatbot/niagarafallsLogo.png",
         "REACT_APP_BOTID": 'CMOTFNPMN6',
-        "REACT_APP_BOTALIASID": 'TSTALIASID',
+        "REACT_APP_BOTALIASID": 'CG0JDQQHIE',
         "siteid": "niagarafalls"
     }
 }
